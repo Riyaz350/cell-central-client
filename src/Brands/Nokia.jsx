c453180909aead961efd653cea7698b996ee7f30
riyaz <riyaz3582@gmail.com>
2023-10-18 20:47:03 +0600

    added navbar and footer

--- src/Brands/Nokia.jsx
+++ src/Brands/Nokia.jsx
@@ -15,45 +15,36 @@ const Nokia = () => {
     return (
         <div>
             
-            <div className=" max-w-7xl mx-auto gap-20 space-y-8 lg:space-y-0  lg:grid grid-cols-2 ">
-            {
-                filtered.map(phone => < >
-                    <div className="lg:w-[400px] ">
-                    <div  className="  bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700">
-                        <div className="lg:max-w-lg  flex justify-center mx-auto">
-                            <img className=" rounded-t-lg w-56" src={phone.photo} alt="product image" />
+        <div className=" max-w-7xl  mx-auto gap-20 space-y-8 lg:space-y-0  lg:grid grid-cols-2 ">
+        {
+            filtered.map(phone => < div key={phone._id} className="">
+                <div className="lg:w-[400px] ">
+                <div  className="  bg-white border border-gray-200 rounded-lg shadow h-[700px] dark:bg-gray-800 dark:border-gray-700">
+                    <div className="lg:max-w-lg  flex justify-center mx-auto">
+                        <img className=" rounded-t-lg w-56" src={phone.photo} alt="product image" />
+                    </div>
+                    <div className=" flex  flex-col justify-between p-10 gap-6 ">
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"><span className="text-xl font-bold">Name:</span> {phone.name}</h5>
+                        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"><span className="text-xl font-bold">Brand:</span> {phone.brand}</h5>
+
+                        <div className="flex items-start gap-6 flex-col">
+                            <p><span className="text-xl font-bold">Type of product:</span> {phone.type}</p>
+                            <span className="bg-blue-100 text-blue-800 text-md font-semibold p-2 rounded dark:bg-blue-200 dark:text-blue-800">Rating: {phone.rating}/5</span>
                         </div>
-                        <div className=" flex  flex-col justify-between p-10 gap-6 ">
-                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{phone.name}</h5>
-                            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">{phone.brand}</h5>
 
+                        <span className="text-3xl font-bold text-gray-900 dark:text-white">Price: <span className="text-[#3e9c35]">{phone.price}</span> TK</span>
 
-
-
-
-
-
-
-                            <div className="flex items-center mt-2.5 mb-5">
-                            
-                                
-
-                                <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">{phone.rating}/5</span>
-                            </div>
-
-                            <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-
-                            <div className="flex items-center justify-between">
-                                <a href="#" className="text-white bg-black hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Show Details</a>
-                                <Link to={`updatePhone/${phone._id}`} className="text-white bg-black hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</Link>
-                            </div>
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="text-white bg-black hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Show Details</a>
+                            <Link to={`updatePhone/${phone._id}`} className="text-white bg-black hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</Link>
                         </div>
                     </div>
-                    </div>
-                    </>)
-            }
-            </div>
+                </div>
+                </div>
+                </div>)
+        }
         </div>
+    </div>
     );
 };
 
